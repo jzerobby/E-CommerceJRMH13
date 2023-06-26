bd67b68d3e076965babfc1dd8b4fb809c23d2045
JZERO MAKILING <jzerorobby07092016@gmail.com>
2023-06-25 23:38:29 -0700

    updated all models and some routes files

---
+++ routes/api/category-routes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  const categoryId = req.params.id;
+  try {
+    const category = await Category.findByPk(categoryId, {
+      include: [{ model: Product }]
+    });
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  const categoryId = req.params.id;
+  try {
+    const category = await Category.findByPk(categoryId);
+    if (!category) {
+      res.status(400).json(err);
+    }
+    await category.update(req.body);
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  const categoryId = req.params.id;
+  try {
+    const category = await Category.findByPk(categoryId);
+    if (!category) {
+      return res.status(400).json(err);
+    }
+    await category.destroy();
+    res.json({ message: 'Category deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
